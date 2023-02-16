431647f53fdc7546336f65eb04194557fc74b5ac
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-02-16 16:16:01 +0800

    added prisma next auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello Next</h1>
+      <h1 className="text-purple-500 text-2xl text-center font-bold">Hello Next</h1>
     </main>
-  )
+  );
 }

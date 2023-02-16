7a997dfa21feac801c7cd2594102849dc3a82231
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-02-16 20:25:48 +0800

    added nav / edited css / setup nextauth

---
+++ app/auth/Nav.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <nav>
+      <Link href={"/"}>
+        <h1>Send it.</h1>
+      </Link>
+    </nav>
+  );
+};
+
+export default Nav;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-purple-500 text-2xl text-center font-bold">Hello Next</h1>
+      <h1 className="text-purple-500 text-2x">Hello Next</h1>
     </main>
   );
 }
